1eb6d2ac6727a85aec695f7333d541af12d26df9
David Hermoso <hermosoj@ryanair.com>
2019-06-13 13:13:58 +0200

    Update readme

--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { MyComponentNameComponent } from './my-component-name/my-component-name.component';
+import { MyComponentComponent } from './my-component/my-component.component';
 
 @NgModule({
-  declarations: [AppComponent, MyComponentNameComponent],
+  declarations: [AppComponent, MyComponentNameComponent, MyComponentComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
